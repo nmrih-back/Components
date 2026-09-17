75f7cc91815840c442c557109369785dc271d228
Real Nego Malandro <158531511+nmrih-back@users.noreply.github.com>
2026-09-17 15:17:35 -0300

    Add main.php with required files

---
+++ main.php
@@ -0,0 +1,8 @@
+<?php
+
+require _once __DIR__ . '/functions/server.php';
+require _once __DIR__ . '/router/router.php';
+require _once __DIR__ . '/middleware/middleware.php';
+require _once __DIR__ . '/dispatcher/dispatcher.php';
+require _once __DIR__ . '/controllers/controllers.php';
+require _once __DIR__ . '/services/services.php';
